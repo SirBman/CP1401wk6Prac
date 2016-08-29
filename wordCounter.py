5788f4734d22f34abaf874defc7b9c3c50e26d78
Ben White <benjamin.white1@my.jcu.edu.au>
2016-08-29 10:45:06 +1000

    first word counter

---
+++ wordCounter.py
@@ -0,0 +1,21 @@
+
+
+strInput = str(input("Please enter a lot of words."))
+
+strInput.strip()
+strInput.lower()
+wordList = strInput.split()
+
+print(wordList)
+
+wordCount = {}
+
+for word in wordList:
+    if word in wordCount:
+        wordCount[word] +=1
+    else:
+        wordCount[word] = 1
+
+
+for word, count in wordCount.items():
+    print("{}: {}".format(word, count))
